5e1fab8fb446a8f68543c6ed958d2d52e0af4dc7
DimAydogdiev <d.aydogdiev@kdteamcompany.com>
2025-12-17 21:06:56 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,7 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {
-            colors: {
-                // Додаємо підтримку Telegram теми (опціонально)
-                tg: {
-                    bg: "var(--tg-theme-bg-color)",
-                    text: "var(--tg-theme-text-color)",
-                    hint: "var(--tg-theme-hint-color)",
-                    link: "var(--tg-theme-link-color)",
-                    button: "var(--tg-theme-button-color)",
-                    buttonText: "var(--tg-theme-button-text-color)",
-                },
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
